02a90d7985d4a08a8051a362e10fdf08fd9fc6c9
Mary Wells <m.a.wells@outlook.com>
2022-03-15 17:37:08 +0000

    add get request

--- server.go
+++ server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
 )
 
@@ -18,11 +19,36 @@ type coasterHandlers struct {
 
 func (h *coasterHandlers) get(w http.ResponseWriter, r *http.Request) {
 
+	coasters := make([]Coaster, len(h.store))
+
+	i := 0
+	for _, coaster := range h.store {
+		coasters[i] = coaster
+		i++
+	}
+
+	jsonBytes, err := json.Marshal(coasters)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+	}
+
+	w.Header().Add("content-type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(jsonBytes)
 }
 
 func newCoasterHandlers() *coasterHandlers {
 	return &coasterHandlers{
-		store: map[string]Coaster{},
+		store: map[string]Coaster{
+			"id01": Coaster{
+				Name:         "Loopy 99",
+				Height:       60,
+				ID:           "id01",
+				InPark:       "Alton Towers",
+				Manufacturer: "Speedranger",
+			},
+		},
 	}
 }
 
