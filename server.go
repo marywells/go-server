7e235133c0ce32e25e90ff5afcd33a6e9715bf1f
Mary Wells <m.a.wells@outlook.com>
2022-03-15 17:14:00 +0000

    repo set-up and server listening

---
+++ server.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"net/http"
+)
+
+type Coaster struct {
+	Name         string `json:"name"`
+	Manufacturer string `json:"manufacturer"`
+	ID           string `json:"id"`
+	InPark       string `json:"in_park"`
+	Height       int    `json:"height"`
+}
+
+type coasterHandlers struct {
+	store map[string]Coaster
+}
+
+func (h *coasterHandlers) get(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func newCoasterHandlers() *coasterHandlers {
+	return &coasterHandlers{
+		store: map[string]Coaster{},
+	}
+}
+
+func main() {
+	coasterHandlers := newCoasterHandlers()
+	http.HandleFunc("/coasters", coasterHandlers.get)
+	err := http.ListenAndServe(":4000", nil)
+
+	if err != nil {
+		panic(err)
+	}
+
+}
