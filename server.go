b479ca19ae099368f015a935df53b58a31f764e8
Mary Wells <m.a.wells@outlook.com>
2022-03-16 09:44:33 +0000

    post request

--- server.go
+++ server.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"encoding/json"
+	"io/ioutil"
 	"net/http"
+	"sync"
 )
 
 type Coaster struct {
@@ -14,18 +16,36 @@ type Coaster struct {
 }
 
 type coasterHandlers struct {
+	sync.Mutex
 	store map[string]Coaster
+}
+
+func (h *coasterHandlers) coasters(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		h.get(w, r)
+		return
+	case "POST":
+		h.post(w, r)
+		return
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		w.Write([]byte("method not allowed"))
+		return
+	}
 }
 
 func (h *coasterHandlers) get(w http.ResponseWriter, r *http.Request) {
 
 	coasters := make([]Coaster, len(h.store))
 
+	h.Lock()
 	i := 0
 	for _, coaster := range h.store {
 		coasters[i] = coaster
 		i++
 	}
+	h.Unlock()
 
 	jsonBytes, err := json.Marshal(coasters)
 	if err != nil {
@@ -36,6 +56,29 @@ func (h *coasterHandlers) get(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("content-type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	w.Write(jsonBytes)
+}
+
+func (h *coasterHandlers) post(w http.ResponseWriter, r *http.Request) {
+	bodyBytes, err := ioutil.ReadAll(r.Body)
+	defer r.Body.Close()
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+	}
+
+	var coaster Coaster
+	err = json.Unmarshal(bodyBytes, &coaster)
+
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(err.Error()))
+	}
+
+	h.Lock()
+	h.store[coaster.ID] = coaster
+	defer h.Unlock()
+
 }
 
 func newCoasterHandlers() *coasterHandlers {
@@ -54,7 +97,7 @@ func newCoasterHandlers() *coasterHandlers {
 
 func main() {
 	coasterHandlers := newCoasterHandlers()
-	http.HandleFunc("/coasters", coasterHandlers.get)
+	http.HandleFunc("/coasters", coasterHandlers.coasters)
 	err := http.ListenAndServe(":4000", nil)
 
 	if err != nil {
